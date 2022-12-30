3f4d46c77c9a271d9ae7251cc2421119968b6676
Tony DE JESUS <tony@dejesus.pt>
2022-12-30 16:21:23 +0100

    miniproject

---
+++ mini-projet/minikube.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+echo "minikute start"
+minikube start
